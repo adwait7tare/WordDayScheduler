9af97ae58e995e38141371fe6ee044a802bd4217
Adwait Tare <adwait7tare@gmail.com>
2021-08-19 11:48:53 -0400

    0819 1148 update

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// create our document redy function to make sure nothing rusn before we load the page
+$(document).ready(function(){
+//All code should go in here ... this will help in ensuring entire code is loaded before it starts executing it
+
+//listen for click events on the page
+onClick function () {
+   //grab the save event options and save user inputs to a variable
+        //create variables to save user input and time
+        var 
+        var
+    // save to localstorage
+
+    // create an alert to alert the user to confirm that the itme has been saved to the local storage
+
+    // create timeout value that removes the notification
+
+}
+
+// create time updater function 
+    // get the current time (moment)
+
+    //create 'if' loop to go over all the time blocks (compare moment time to html page showing time in the time block ... each time block has specific id)
+
+    // if else if statement to 
+    // ().removeClass() or ().addClass()
+        // check to see if we have passed our time
+            // check out removeClass and addClass
+        // check to see if current hour matches hour included in the box on html
+
+        // check to see if time time is in the future
+
+// retrive localstorage and render items to the correct rows
+
+//(get one of the rows working and then work on adding other rows)
+
+
+
+});
